a0151798aa7cbda40a847bdb5a73dce138237a8f
khaled hizawi <khaledhizawi@gmail.com>
2021-11-30 19:04:17 +0100

    Digits

--- com/src/main/java/Digits.java
+++ com/src/main/java/Digits.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Digits {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int input=scanner.nextInt();
+            if (input==0) System.out.println(0);
+            int res=0;
+            while (input>0){
+
+                res=input%10;
+                System.out.print(res+" ");
+                input/=10;
+            }
+        }
+
+
+   /*     System.out.println(123 %10);
+        System.out.println(123 /10);*/
+    }
 }
+// 123
